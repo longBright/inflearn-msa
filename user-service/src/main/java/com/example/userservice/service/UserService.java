1d8c9752c0801a1c021af19350f09fa9055d6bc9
최영환 <longbright0804@gmail.com>
2023-08-30 17:57:47 +0900

    08.30

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -3,18 +3,24 @@ package com.example.userservice.service;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.User;
 import com.example.userservice.jpa.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
+import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static org.modelmapper.convention.MatchingStrategies.STRICT;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class
+UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -29,5 +35,24 @@ public class UserService {
         user.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(user);
+    }
+
+    public ResponseUser getUserByUserId(String userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user == null) {
+            throw new NoSuchElementException("User Not Found");
+        }
+        ResponseUser response = new ModelMapper().map(user, ResponseUser.class);
+        List<ResponseOrder> orders = new ArrayList<>();
+        response.setOrders(orders);
+        return response;
+    }
+
+    public List<ResponseUser> getUserByAll() {
+        List<ResponseUser> users = new ArrayList<>();
+        userRepository.findAll().forEach(v -> {
+            users.add(new ModelMapper().map(v, ResponseUser.class));
+        });
+        return users;
     }
 }
